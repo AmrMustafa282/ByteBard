6d17591dfa3182f9495bf671be0a1e6e12903799
AmrMustafa <90666301+AmrMustafa282@users.noreply.github.com>
2024-01-29 20:14:35 +0200

    starter-files

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+ return (
+  <div className="bg-black">
+   <div className="container mx-auto h-screen bg-red-600">
+    <div className="bg-blue-500 ">amr</div>
+   </div>
+  </div>
+ );
+}
+
+export default App;
