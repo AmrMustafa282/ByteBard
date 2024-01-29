4f7d72b6fbe0f56b0535683bc501bf5dce5877fa
AmrMustafa <90666301+AmrMustafa282@users.noreply.github.com>
2024-01-29 20:40:49 +0200

    pages & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+import Home from"./pages/Home"
+import About from"./pages/About"
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignUp from"./pages/SignUp"
+import SignIn from"./pages/SignIn"
+
 function App() {
- return (
-  <div className="bg-black">
-   <div className="container mx-auto h-screen bg-red-600">
-    <div className="bg-blue-500 ">amr</div>
-   </div>
-  </div>
+  return (
+   
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='about' element={<About/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='*' element={<div>404</div>} />
+      </Routes>
+    </Router>
  );
 }
 
