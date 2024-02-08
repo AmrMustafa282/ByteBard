d3610d946fae71f69a25288feffbab22aab39729
Amr <amr@Amrs-MacBook-Pro.local>
2024-02-08 04:14:34 +0200

    ac

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+import Home from"./pages/Home"
+import About from"./pages/About"
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignUp from"./pages/SignUp"
+import SignIn from"./pages/SignIn"
+import Header from "./components/Header";
+
+function App() {
+  return (
+   
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='about' element={<About/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='*' element={<div>404</div>} />
+      </Routes>
+    </Router>
+ );
+}
+
+export default App;
