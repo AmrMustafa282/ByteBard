6eff1f2fddd8c6686437cd91bbaa82fe72d67f82
Amr <amr@192.168.1.8>
2024-02-18 00:32:45 +0200

    Signup funs

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 import { Button, Navbar, TextInput } from "flowbite-react";
-import logo from "../assets/logo.png";
+
 
 const Header = () => {
  const path = useLocation().pathname;
