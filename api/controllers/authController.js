696e31545be339d7127dcf43451ddd5db6b9ad7a
Amr <amr@192.168.1.4>
2024-02-21 16:46:08 +0200

    signup route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,6 +2,8 @@
 
 import User from "../models/userModel.js";
 import { errorHandler } from "../utils/error.js";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 
 export const signup = async (req, res, next) => {
@@ -21,3 +23,31 @@ export const signup = async (req, res, next) => {
     next(err)
   }
 }
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    next(errorHandler(400,'Please provide email and password!'))
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+     return next(errorHandler(400, "Invalid email or password"));
+    }
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+     return next(errorHandler(400, "Invalid email or password"));
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      {expiresIn:'1h'}
+    )
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).cookie('jwt', token, {
+      httpOnly: true,
+    }).json(rest);
+  } catch (err) {
+    next(err);
+  }
+} 
