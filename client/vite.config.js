95c125e319e3aab1941f15c85bf692a908676c93
AmrMustafa282 <amrdarwish115@gmail.com>
2024-03-05 00:54:15 +0200

    changeOrigin to render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bytebard-hfri.onrender.com/',
-        secure:false
-      },  
-    },
+ server: {
+  proxy: {
+   "/api": {
+    target: "https://bytebard-hfri.onrender.com",
+    changeOrigin: true, // Add this line if necessary
+    secure: true,
+   },
   },
-  plugins: [react()],
-})
+ },
+ plugins: [react()],
+});
