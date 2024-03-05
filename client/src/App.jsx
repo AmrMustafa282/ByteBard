d63aa7b0227078bf0bf4b55b4eb99c9475d9cd85
AmrMustafa282 <amrdarwish115@gmail.com>
2024-03-05 02:27:49 +0200

    privateRoute component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,32 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
-import Home from"./pages/Home"
-import About from"./pages/About"
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
-import SignUp from"./pages/SignUp"
-import SignIn from"./pages/SignIn"
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
 import FooterFC from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return (
-   
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='about' element={<About/>} />
-        <Route path='/projects' element={<Projects/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/sign-up' element={<SignUp/>} />
-        <Route path='/sign-in' element={<SignIn/>} />
-        <Route path='*' element={<div>404</div>} />
-      </Routes>
-      <FooterFC />
-    </Router>
+ return (
+  <Router>
+   <Header />
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route element={<PrivateRoute />}>
+     <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="*" element={<div>404</div>} />
+   </Routes>
+   <FooterFC />
+  </Router>
  );
 }
 
