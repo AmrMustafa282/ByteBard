584968083279662bc8931f31c2ec04715fdb64e3
AmrMustafa <amrdarwish115@gmail.com>
2024-03-09 06:03:08 +0200

    getusers api

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
  updateUser,
  deleteUser,
  signout,
+ getUsers,
 } from "./../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,6 +14,7 @@ const router = express.Router();
 router.put('/update/:userId',verifyToken,updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
 router.post('/signout', signout)
+router.get('/getusers',verifyToken,getUsers)
 
 
 
