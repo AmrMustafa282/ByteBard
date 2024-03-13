e5c45adfa9065e4e2c7ddc08c1db92f854142228
AmrMustafa <amrdarwish115@gmail.com>
2024-03-13 22:35:47 +0200

    render upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
  server: {
   proxy: {
    "/api": {
-    target: "http://localhost:3000",
-    // target: "https://bytebard-hfri.onrender.com",
+    // target: "http://localhost:3000",
+    target: "https://bytebard-hfri.onrender.com",
     changeOrigin: true, // Add this line if necessary
     secure: true,
    },
